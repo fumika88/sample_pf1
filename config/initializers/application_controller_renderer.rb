d26ae374fff300cee3a8c105d6f3974134c7ec76
fumika88 <fuum14532@gmail.com>
2021-07-14 15:01:11 +0000

    image追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f436b225ce419ec774b9bd0a16a3a1cef406efcd50d8176a15c5a299802aca90cca2799ef83e91b54a21236b65af92b655b94f2c2fb1379981038064c8cf685a'
